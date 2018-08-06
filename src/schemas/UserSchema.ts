ccbf620c89f5453259bd84e6b3611987c3119a86
GSE995 <gorynovse@gmail.com>
2018-08-06 23:39:08 +0300

    add user schema

---
+++ src/schemas/UserSchema.ts
@@ -0,0 +1,40 @@
+import * as mongoose from "mongoose"
+import * as bcrypt from 'bcrypt'
+
+const UserSchema = new mongoose.Schema({
+	username: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+	}
+})
+
+UserSchema.pre('save', function (next) {
+    const user = this;
+    if (this.isModified('password') || this.isNew) {
+      bcrypt.genSalt(10, (error, salt) => {
+      if (error) return next(error);
+      bcrypt.hash(user.password, salt, (error, hash) => {
+        if (error) return next(error);
+        user.password = hash;
+          next();
+        });
+      });
+    } else {
+      return next();
+    }
+  });
+
+
+
+UserSchema.methods.comparePassword = async function(password, callback) : Promise<any>{
+	return await bcrypt.compare(password, this.password);
+}
+
+const User = mongoose.model("User", UserSchema);
+
+export default User;
