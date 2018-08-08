5c214d619054d9355f5a86dedb75db0e9f0d2486
GSE995 <gorynovse@gmail.com>
2018-08-08 22:30:56 +0300

    add database config and add controllers

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,17 @@ import * as bodyParser from 'body-parser'
 import * as passport from 'passport'
 import * as morgan from 'morgan'
 
+import config from './config'
+import database from './config/database'
+
+import AddControllers from './config/controllers'
+
 const BudgetManagerPORT = process.env.PORT || 3001;
 
 const app = express();
+
+database(mongoose, config)
+
 
 app.use(express.static('.'));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -16,6 +24,8 @@ app.use(bodyParser.json());
 app.use(passport.initialize());
 app.use(morgan('combined'))
 app.use(bodyParser.json())
+
+AddControllers(app)
 
 app.listen(BudgetManagerPORT, () => {
 	console.log(`Budget manager api running on ${BudgetManagerPORT}`)
