20da118bf588d06ce350ea59ddf66e92bbf5e752
GSE995 <gorynovse@gmail.com>
2018-08-08 22:33:10 +0300

    add type Express, Request, Response

--- src/controllers/UserControllers.ts
+++ src/controllers/UserControllers.ts
@@ -1,17 +1,18 @@
 import {UserService} from "../services/UserService"
 import {ErroResult} from '../common/Result'
+import {Express, Response, Request} from 'express'
 
-export default (app) => {
+export default (app: Express) => {
 	app.route('/api/v1/signup')
-	   .post(async (req, res) => {
+	   .post(async (req: Request, res: Response) => {
 			if (!req.body.username || !req.body.password) res.status(400).json(new ErroResult('Please, pass an username and password.'));
-
+			
 			try {
 				let result = await UserService.signup(req.body.username, req.body.password)
 				res.status(200).json(result)
 			} catch (error) {
-				
+				console.log(error);
 			}
 			res.status(500).send()
 	   })
-  }
+}
