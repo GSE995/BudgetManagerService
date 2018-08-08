d343ce8a9477cce7c14fca23f89756af61b14f95
GSE995 <gorynovse@gmail.com>
2018-08-08 23:03:34 +0300

    add auth service

---
+++ src/services/AuthService.ts
@@ -0,0 +1,21 @@
+import UserSchema from '../schemas/UserSchema'
+import {Result, SuccessResult, ErroResult} from '../common/Result'
+import * as jwt from 'jsonwebtoken'
+import config from '../config'
+
+export class AuthService {
+	static async login(username: string, password: string) : Promise<Result<any>>{
+		let user: any = await UserSchema.findOne({ username})
+		if(!user){
+			return new ErroResult('Authentication failed. User not found.')
+		}
+		
+		let match = await user.comparePassword(password)
+
+		if(match){
+			const token = jwt.sign({user}, config.secret)
+			return new SuccessResult('Token granted', {token, user})
+		}
+		return new ErroResult('Authentication failed. Wrong password.', )
+	}
+}
