81ed8fd81ff6ebebb9f3ef1be31fc0a7d29747f3
GSE995 <gorynovse@gmail.com>
2018-08-08 22:33:31 +0300

    upd

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,14 +1,13 @@
 import User from '../schemas/UserSchema'
 import {Result, SuccessResult, ErroResult} from '../common/Result'
 
-export class UserService {
-  static async signup (username: string, password: string): Promise<Result<any>>{
-    const user = new User({
-      username: username,
-      password: password
-    })
-
-    let result = await user.save()
-    return new SuccessResult()
-  }
+export class UserService{
+  	static async signup (username: string, password: string): Promise<Result<any>>{
+		const user = new User({
+			username,
+			password
+		})
+		let result = await user.save()
+		return new SuccessResult()
+	}
 }
