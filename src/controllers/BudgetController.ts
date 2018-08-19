c0079b614681c8f4ef8aa991d2066e3abc000e4d
GSE995 <gorynovse@gmail.com>
2018-08-19 20:28:40 +0300

    upd budget controller

--- src/controllers/BudgetController.ts
+++ src/controllers/BudgetController.ts
@@ -5,39 +5,39 @@ export default (app: Express) : void => {
     app.route('/api/v1/budget/:id')
         .get(async (req: Request, res: Response)=> {
             try {
-				let result = await BudgetService.saveAsync(req.body)
+                let result = await BudgetService.saveAsync(req.body)
 
                 res.send(result)
-                	
-			} catch (error) {
-				// todo: logger
-			}
-			res.status(500)
+                    
+            } catch (error) {
+                // todo: logger
+            }
+            res.status(500)
         })
         .post(async (req: Request, res: Response) => {
             try {
-				if(!req.params.id) res.status(404)
-				
-				let result = await BudgetService.getByIdAsync(req.params.id)
+                if(!req.params.id) res.status(404)
+                
+                let result = await BudgetService.getByIdAsync(req.params.id)
 
                 res.send(result)
                 
-			} catch (error) {
-				// todo: logger
-			}
-			res.status(500)
+            } catch (error) {
+                // todo: logger
+            }
+            res.status(500)
         })
         .delete(async (req: Request, res: Response) => {
             try {
-				
-				let result = await BudgetService.removeAsync(req.query._id)
+                
+                let result = await BudgetService.removeAsync(req.query._id)
 
-				res.send(result)
-				
-			} catch (error) {
-				// todo: logger
-			}
+                res.send(result)
+                
+            } catch (error) {
+                // todo: logger
+            }
 
-			res.status(500)
+            res.status(500)
         })
 }
