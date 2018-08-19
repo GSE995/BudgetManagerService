a236280cb31d06b9eae5e42fd8fdd4b4cf09e202
GSE995 <gorynovse@gmail.com>
2018-08-19 20:21:27 +0300

    add budget controller

---
+++ src/controllers/BudgetController.ts
@@ -0,0 +1,43 @@
+import BudgetService from '../services/BudgetService'
+import {Request, Response, Express} from 'express'
+
+export default (app: Express) : void => {
+    app.route('/api/v1/budget/:id')
+        .get(async (req: Request, res: Response)=> {
+            try {
+				let result = await BudgetService.saveAsync(req.body)
+
+                res.send(result)
+                	
+			} catch (error) {
+				// todo: logger
+			}
+			res.status(500)
+        })
+        .post(async (req: Request, res: Response) => {
+            try {
+				if(!req.params.id) res.status(404)
+				
+				let result = await BudgetService.getByIdAsync(req.params.id)
+
+                res.send(result)
+                
+			} catch (error) {
+				// todo: logger
+			}
+			res.status(500)
+        })
+        .delete(async (req: Request, res: Response) => {
+            try {
+				
+				let result = await BudgetService.removeAsync(req.query._id)
+
+				res.send(result)
+				
+			} catch (error) {
+				// todo: logger
+			}
+
+			res.status(500)
+        })
+}
