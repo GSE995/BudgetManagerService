c2d2d1b4f6798892fabc42bea5710402de9d5e7f
GSE995 <gorynovse@gmail.com>
2018-08-20 12:21:27 +0300

    add budget and client controllers

--- src/config/controllers.ts
+++ src/config/controllers.ts
@@ -1,8 +1,12 @@
 import UserController from '../controllers/UserControllers'
 import AuthController from '../controllers/AuthController'
+import BudgetController from '../controllers/BudgetController'
+import ClientController from '../controllers/ClientController'
 import {Express} from 'express'
 
 export default (app: Express) => {
 	UserController(app)
 	AuthController(app)
+	BudgetController(app)
+	ClientController(app)
 }
