8fb7c045cd8cc65c482efef37b51b3d870ffd4d8
GSE995 <gorynovse@gmail.com>
2018-09-15 23:36:15 +0300

    add use my cors config

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import * as express from 'express'
 import * as parser from 'body-parser'
-import * as cors from 'cors'
+// import * as cors from 'cors'
 import * as mongoose from 'mongoose'
 import * as bodyParser from 'body-parser'
 import * as passport from 'passport'
@@ -9,6 +9,7 @@ import * as morgan from 'morgan'
 import config from './config'
 import database from './config/database'
 import passportConfig from './config/passport'
+import UseCors from './config/cors'
 
 import AddControllers from './config/controllers'
 
@@ -20,13 +21,17 @@ database(mongoose, config)
 passportConfig(passport)
 
 
+
 app.use(express.static('.'));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
 app.use(passport.initialize());
 app.use(morgan('combined'))
 app.use(bodyParser.json())
+// app.use(cors)
 
+UseCors(app)
+
+app.set('budgetsecret', config.secret)
 AddControllers(app)
 
 app.listen(BudgetManagerPORT, () => {
