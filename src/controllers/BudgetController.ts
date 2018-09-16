a244d9810ae52ce4c6b83aa0a9337edcd45c8149
GSE995 <gorynovse@gmail.com>
2018-09-16 23:50:56 +0300

    upd BudgetController: add router to get a list budgets

--- src/controllers/BudgetController.ts
+++ src/controllers/BudgetController.ts
@@ -5,6 +5,9 @@ import { Budget } from '../models/Budget'
 import * as passport from 'passport'
 import config from '../config/index'
 import logger from '../config/logger'
+import PageParameter from '../common/PageParameter';
+import BudgetFilter from '../schemas/filters/BudgetFilter';
+import FilterFactory from '../schemas/filters/FilterFactory';
 
 export default (app: Express) : void => {
 
@@ -30,11 +33,11 @@ export default (app: Express) : void => {
 
                 let budgetDto = deserialize(Budget, req.body)
                 let result = BudgetService.saveAsync(budgetDto)
-                
+                res.send(result)
             } catch (error) {
                 logger.error(error)
             }
-            res.status(500)
+            res.status(500).send()
         })
         .delete(async (req: Request, res: Response) => {
             try {
@@ -49,4 +52,24 @@ export default (app: Express) : void => {
 
             res.status(500)
         })
+
+    app.route('/api/v1/budget/list/')
+		.all(passport.authenticate('jwt', config.session), (req: Request, res: Response, next) => {
+			if(!app.get('budgetsecret')) res.status(403).send()
+			next()
+		})
+		.get(async (req: Request, res: Response) => {
+
+            let parameter = new PageParameter(req.query.limit, req.query.skip)
+			let filter = FilterFactory.create<BudgetFilter>(BudgetFilter, req.query)
+
+			try {
+                let result = await BudgetService.getListAsync(filter, parameter)
+				if(!result.items)res.status(404).send()
+				res.send(result)
+			} catch (error) {
+                logger.error(error)
+			}
+			res.status(500).send()
+		})
 }
