bcef7bcc604a3f921a1ee21b4deeba5af8d9fdb0
GSE995 <gorynovse@gmail.com>
2018-09-17 00:11:15 +0300

    upd ClientController: add router to get a list clients

--- src/controllers/ClientController.ts
+++ src/controllers/ClientController.ts
@@ -5,6 +5,9 @@ import { deserialize } from 'json-typescript-mapper'
 import * as passport from 'passport'
 import config from '../config/index'
 import logger from '../config/logger'
+import FilterFactory from '../schemas/filters/FilterFactory';
+import PageParameter from '../common/PageParameter';
+import ClientFilter from '../schemas/filters/ClientFilter';
 
 export default (app: Express) => {
 	app.route('/api/v1/client')
@@ -66,4 +69,24 @@ export default (app: Express) => {
 
 			res.status(500).send()
 		})
+
+		app.route('/api/v1/client/list/')
+			.all(passport.authenticate('jwt', config.session), (req, res, next) => {
+				if(!app.get('budgetsecret')) res.status(403).send()
+				next()
+			})
+			.get(async (req: Request, res: Response) => {
+
+				let parameter = new PageParameter(req.query.limit, req.query.skip)
+				let filter = FilterFactory.create<ClientFilter>(ClientFilter, req.query)
+
+				try {
+					let result = await ClientService.getListAsync(filter, parameter)
+					if(!result.items)res.status(404).send()
+					res.send(result)
+				} catch (error) {
+					logger.error(error)
+				}
+				res.status(500).send()
+			})
 }
